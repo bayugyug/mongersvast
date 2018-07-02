de037db176c8810fe3d754cff683000db0c938ca
bayugyug <mtejero@artofclick.com>
2018-07-02 15:36:46 +0800

    - sync ...

--- adsget.go
+++ adsget.go
@@ -29,6 +29,7 @@ func (v *VAST) GetAdPos() int {
 	return 0
 }
 
+//GetAdsInLine get the list of all inline
 func (v *VAST) GetAdsInLine() []*InLine {
 	//minimal config
 	if v == nil {
@@ -45,6 +46,7 @@ func (v *VAST) GetAdsInLine() []*InLine {
 	return all
 }
 
+//GetAdsWrapper get the list of all wrapper
 func (v *VAST) GetAdsWrapper() []*Wrapper {
 	//minimal config
 	if v == nil {
@@ -56,6 +58,27 @@ func (v *VAST) GetAdsWrapper() []*Wrapper {
 	//just in case
 	for _, vv := range v.Ad {
 		all = append(all, vv.Wrapper)
+	}
+	//good ;-)
+	return all
+}
+
+//GetAdsAdSystem get the list of all AdSystem
+func (v *VAST) GetAdsAdSystem() []*AdSystem {
+	//minimal config
+	if v == nil {
+		v = &VAST{
+			Version: VastXMLVer2,
+		}
+	}
+	var all []*AdSystem
+	//just in case
+	for _, vv := range v.Ad {
+		if vv.InLine != nil && vv.InLine.AdSystem != nil {
+			all = append(all, vv.InLine.AdSystem)
+		} else if vv.Wrapper != nil && vv.Wrapper.AdSystem != nil {
+			all = append(all, vv.Wrapper.AdSystem)
+		}
 	}
 	//good ;-)
 	return all
