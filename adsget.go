de8ee13df9cdc489d02074dfd120ca20452742ad
bayugyug <mtejero@artofclick.com>
2018-07-02 16:06:39 +0800

    - sync ...

--- adsget.go
+++ adsget.go
@@ -150,3 +150,269 @@ func (v *VAST) GetAdsDescription() map[string][]*Description {
 	//good ;-)
 	return all
 }
+
+//GetAdsErrorURL get the list of all VASTError
+func (v *VAST) GetAdsErrorURL() map[string][]*VASTError {
+	//minimal config
+	if v == nil {
+		v = &VAST{
+			Version: VastXMLVer2,
+		}
+	}
+	var all map[string][]*VASTError
+	all = make(map[string][]*VASTError)
+	//just in case
+	for _, vv := range v.Ad {
+		if vv.InLine != nil && vv.InLine.VASTError != nil {
+			all[AdTypeIsInline] = append(all[AdTypeIsInline], vv.InLine.VASTError)
+		} else if vv.Wrapper != nil && vv.Wrapper.VASTError != nil {
+			all[AdTypeIsWrapper] = append(all[AdTypeIsWrapper], vv.Wrapper.VASTError)
+		}
+	}
+	//good ;-)
+	return all
+}
+
+//GetAdsSurvey get the list of all Survey
+func (v *VAST) GetAdsSurvey() map[string][]*Survey {
+	//minimal config
+	if v == nil {
+		v = &VAST{
+			Version: VastXMLVer2,
+		}
+	}
+	var all map[string][]*Survey
+	all = make(map[string][]*Survey)
+	//just in case
+	for _, vv := range v.Ad {
+		if vv.InLine != nil && vv.InLine.Survey != nil {
+			all[AdTypeIsInline] = append(all[AdTypeIsInline], vv.InLine.Survey)
+		} else if vv.Wrapper != nil && vv.Wrapper.Survey != nil {
+			all[AdTypeIsWrapper] = append(all[AdTypeIsWrapper], vv.Wrapper.Survey)
+		}
+	}
+	//good ;-)
+	return all
+}
+
+//GetAdsExpires get the list of all Expires
+func (v *VAST) GetAdsExpires() map[string][]*Expires {
+	//minimal config
+	if v == nil {
+		v = &VAST{
+			Version: VastXMLVer2,
+		}
+	}
+	var all map[string][]*Expires
+	all = make(map[string][]*Expires)
+	//just in case
+	for _, vv := range v.Ad {
+		if vv.InLine != nil && vv.InLine.Expires != nil {
+			all[AdTypeIsInline] = append(all[AdTypeIsInline], vv.InLine.Expires)
+		} else if vv.Wrapper != nil && vv.Wrapper.Expires != nil {
+			all[AdTypeIsWrapper] = append(all[AdTypeIsWrapper], vv.Wrapper.Expires)
+		}
+	}
+	//good ;-)
+	return all
+}
+
+//GetAdsCategory get the list of all Category
+func (v *VAST) GetAdsCategory() map[string][]*Category {
+	//minimal config
+	if v == nil {
+		v = &VAST{
+			Version: VastXMLVer2,
+		}
+	}
+	var all map[string][]*Category
+	all = make(map[string][]*Category)
+	//just in case
+	for _, vv := range v.Ad {
+		if vv.InLine != nil && vv.InLine.Category != nil {
+			all[AdTypeIsInline] = append(all[AdTypeIsInline], vv.InLine.Category)
+		} else if vv.Wrapper != nil && vv.Wrapper.Category != nil {
+			all[AdTypeIsWrapper] = append(all[AdTypeIsWrapper], vv.Wrapper.Category)
+		}
+	}
+	//good ;-)
+	return all
+}
+
+//GetAdsAdvertiser get the list of all Advertiser
+func (v *VAST) GetAdsAdvertiser() map[string][]*Advertiser {
+	//minimal config
+	if v == nil {
+		v = &VAST{
+			Version: VastXMLVer2,
+		}
+	}
+	var all map[string][]*Advertiser
+	all = make(map[string][]*Advertiser)
+	//just in case
+	for _, vv := range v.Ad {
+		if vv.InLine != nil && vv.InLine.Advertiser != nil {
+			all[AdTypeIsInline] = append(all[AdTypeIsInline], vv.InLine.Advertiser)
+		} else if vv.Wrapper != nil && vv.Wrapper.Advertiser != nil {
+			all[AdTypeIsWrapper] = append(all[AdTypeIsWrapper], vv.Wrapper.Advertiser)
+		}
+	}
+	//good ;-)
+	return all
+}
+
+//GetAdsPricing get the list of all Pricing
+func (v *VAST) GetAdsPricing() map[string][]*Pricing {
+	//minimal config
+	if v == nil {
+		v = &VAST{
+			Version: VastXMLVer2,
+		}
+	}
+	var all map[string][]*Pricing
+	all = make(map[string][]*Pricing)
+	//just in case
+	for _, vv := range v.Ad {
+		if vv.InLine != nil && vv.InLine.Pricing != nil {
+			all[AdTypeIsInline] = append(all[AdTypeIsInline], vv.InLine.Pricing)
+		} else if vv.Wrapper != nil && vv.Wrapper.Pricing != nil {
+			all[AdTypeIsWrapper] = append(all[AdTypeIsWrapper], vv.Wrapper.Pricing)
+		}
+	}
+	//good ;-)
+	return all
+}
+
+//GetAdsImpression get the list of all Impression
+func (v *VAST) GetAdsImpression() map[string][]*Impression {
+	//minimal config
+	if v == nil {
+		v = &VAST{
+			Version: VastXMLVer2,
+		}
+	}
+	var all map[string][]*Impression
+	all = make(map[string][]*Impression)
+	//just in case
+	for _, vv := range v.Ad {
+		if vv.InLine != nil && vv.InLine.Impression != nil {
+			all[AdTypeIsInline] = append(all[AdTypeIsInline], vv.InLine.Impression)
+		} else if vv.Wrapper != nil && vv.Wrapper.Impression != nil {
+			all[AdTypeIsWrapper] = append(all[AdTypeIsWrapper], vv.Wrapper.Impression)
+		}
+	}
+	//good ;-)
+	return all
+}
+
+//GetAdsVASTAdTagURI get the list of all VASTAdTagURI
+func (v *VAST) GetAdsVASTAdTagURI() map[string][]*VASTAdTagURI {
+	//minimal config
+	if v == nil {
+		v = &VAST{
+			Version: VastXMLVer2,
+		}
+	}
+	var all map[string][]*VASTAdTagURI
+	all = make(map[string][]*VASTAdTagURI)
+	//just in case
+	for _, vv := range v.Ad {
+		if vv.Wrapper != nil && vv.Wrapper.VASTAdTagURI != nil {
+			all[AdTypeIsWrapper] = append(all[AdTypeIsWrapper], vv.Wrapper.VASTAdTagURI)
+		}
+	}
+	//good ;-)
+	return all
+}
+
+//GetAdsAdVerifications get the list of all AdVerifications
+func (v *VAST) GetAdsAdVerifications() map[string][]*AdVerifications {
+	//minimal config
+	if v == nil {
+		v = &VAST{
+			Version: VastXMLVer2,
+		}
+	}
+	var all map[string][]*AdVerifications
+	all = make(map[string][]*AdVerifications)
+	//just in case
+	for _, vv := range v.Ad {
+		if vv.InLine != nil && vv.InLine.AdVerifications != nil {
+			all[AdTypeIsInline] = append(all[AdTypeIsInline], vv.InLine.AdVerifications)
+		} else if vv.Wrapper != nil && vv.Wrapper.AdVerifications != nil {
+			all[AdTypeIsWrapper] = append(all[AdTypeIsWrapper], vv.Wrapper.AdVerifications)
+		}
+	}
+
+	//good ;-)
+	return all
+}
+
+//GetAdsExtensions get the list of all Extensions
+func (v *VAST) GetAdsExtensions() map[string][]*Extensions {
+	//minimal config
+	if v == nil {
+		v = &VAST{
+			Version: VastXMLVer2,
+		}
+	}
+	var all map[string][]*Extensions
+	all = make(map[string][]*Extensions)
+	//just in case
+	for _, vv := range v.Ad {
+		if vv.InLine != nil && vv.InLine.Extensions != nil {
+			all[AdTypeIsInline] = append(all[AdTypeIsInline], vv.InLine.Extensions)
+		} else if vv.Wrapper != nil && vv.Wrapper.Extensions != nil {
+			all[AdTypeIsWrapper] = append(all[AdTypeIsWrapper], vv.Wrapper.Extensions)
+		}
+	}
+
+	//good ;-)
+	return all
+}
+
+//GetAdsViewableImpression get the list of all ViewableImpression
+func (v *VAST) GetAdsViewableImpression() map[string][]*ViewableImpression {
+	//minimal config
+	if v == nil {
+		v = &VAST{
+			Version: VastXMLVer2,
+		}
+	}
+	var all map[string][]*ViewableImpression
+	all = make(map[string][]*ViewableImpression)
+	//just in case
+	for _, vv := range v.Ad {
+		if vv.InLine != nil && vv.InLine.ViewableImpression != nil {
+			all[AdTypeIsInline] = append(all[AdTypeIsInline], vv.InLine.ViewableImpression)
+		} else if vv.Wrapper != nil && vv.Wrapper.ViewableImpression != nil {
+			all[AdTypeIsWrapper] = append(all[AdTypeIsWrapper], vv.Wrapper.ViewableImpression)
+		}
+	}
+
+	//good ;-)
+	return all
+}
+
+//GetAdsCreatives get the list of all Creatives
+func (v *VAST) GetAdsCreatives() map[string][]*Creatives {
+	//minimal config
+	if v == nil {
+		v = &VAST{
+			Version: VastXMLVer2,
+		}
+	}
+	var all map[string][]*Creatives
+	all = make(map[string][]*Creatives)
+	//just in case
+	for _, vv := range v.Ad {
+		if vv.InLine != nil && vv.InLine.Creatives != nil {
+			all[AdTypeIsInline] = append(all[AdTypeIsInline], vv.InLine.Creatives)
+		} else if vv.Wrapper != nil && vv.Wrapper.Creatives != nil {
+			all[AdTypeIsWrapper] = append(all[AdTypeIsWrapper], vv.Wrapper.Creatives)
+		}
+	}
+
+	//good ;-)
+	return all
+}
